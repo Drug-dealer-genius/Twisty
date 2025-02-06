d9dfa6833a44b6fbd89ae9880832d85ca8b81d87
Drug-dealer-genius <benabdallahfiras833@gmail.com>
2025-02-06 21:57:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Example product data for Twisty website
 const products = [
-    { id: 1, name: "Black Jacket", price: "$49.99", image: "images/black-jacket.jpg" },
-    { id: 2, name: "White Sneakers", price: "$69.99", image: "images/white-sneakers.jpg" },
+    { id: 1, name: "Krunker", price: "$49.99", image: "images/black-jacket.jpg" },
+    { id: 2, name: "Masson", price: "$69.99", image: "images/white-sneakers.jpg" },
     { id: 3, name: "Blue Jeans", price: "$39.99", image: "images/blue-jeans.jpg" },
     { id: 4, name: "Red Dress", price: "$59.99", image: "images/red-dress.jpg" }
 ];
